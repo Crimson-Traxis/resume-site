ffca2df2029f81cce360b221b52c0300a3b3c1f8
Crimson-Traxis <trentkillinger@gmail.com>
2018-11-05 00:57:34 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { 
+  ButtonDropdown, 
+  DropdownToggle, 
+  DropdownMenu, 
+  DropdownItem,
+  Button,
+  Container, 
+  Row, 
+  Col } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  } from '@fortawesome/free-solid-svg-icons'
+import { 
+  faLinkedin,
+  faGithubSquare,
+  faDev } from '@fortawesome/free-brands-svg-icons'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+import {
+  jarallax,
+} from 'jarallax';
+import Typed from 'react-typed';
+import headshot from './images/headshot.jpg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      navDropdownOpen: false
+    };
+  }
+
+  toggle = () => {
+    this.setState({
+      navDropdownOpen: !this.state.navDropdownOpen
+    });
+  }
+
+  navigate = (url) => {
+    window.location = url;
+  }
+
+  navigateNewTab = (url) => {
+    window.open(url);
+  }
+
+  componentDidMount() {
+    jarallax(document.querySelectorAll('.jarallax'));
+  }
+
+  componentWillUnmount() {
+   jarallax(document.querySelectorAll('.jarallax'), 'destroy');
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header id="header" className="App-header jarallax">
+          <img src={headshot} className="headshot" alt="headshot" />
+          <div className="basic-info">
+            <Row>
+              <Col>
+                <h2>Trent Killinger</h2>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Typed 
+                  strings={[
+                      '.Net Developer',
+                      'Mobile Developer',
+                      'Software Analyst']}
+                      typeSpeed={40}
+                      backSpeed={50}
+                      loop />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <ul className="info-resources">
+                  <li><Button outline color="primary" onClick={() => { this.navigateNewTab('https://www.linkedin.com/in/trent-killinger-681252115/')} } ><FontAwesomeIcon icon={faLinkedin} />&nbsp;LinkedIn</Button></li>
+                  <li><Button outline color="primary" onClick={() => { this.navigateNewTab('https://devpost.com/Crimson-Traxis')} } ><FontAwesomeIcon icon={faDev} />&nbsp;Devpost</Button></li>
+                  <li><Button outline color="primary" onClick={() => { this.navigateNewTab('https://github.com/Crimson-Traxis')} } ><FontAwesomeIcon icon={faGithubSquare} />&nbsp;Github</Button></li>
+                </ul>
+              </Col>
+            </Row>
+          </div>
         </header>
+        <ButtonDropdown isOpen={this.state.navDropdownOpen} toggle={() => {this.toggle()} } className="navigation-dropdown">
+          <DropdownToggle caret outline color="primary">
+            Navigation
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem header>Home</DropdownItem>
+            <AnchorLink className="dropdown-item" href='#header' onClick={() => { this.setState({navDropdownOpen:false}) } }>Intro</AnchorLink>
+            <AnchorLink className="dropdown-item" href='#header' onClick={() => { this.setState({navDropdownOpen:false}) } }>Education</AnchorLink>
+            <AnchorLink className="dropdown-item" href='#header' onClick={() => { this.setState({navDropdownOpen:false}) } }>Experience</AnchorLink>
+            <AnchorLink className="dropdown-item" href='#header' onClick={() => { this.setState({navDropdownOpen:false}) } }>Achievements</AnchorLink>
+            <AnchorLink className="dropdown-item" href='#header' onClick={() => { this.setState({navDropdownOpen:false}) } }>Skills</AnchorLink>
+            <DropdownItem divider />
+            <DropdownItem header>Extra</DropdownItem>
+            <DropdownItem>Other Links</DropdownItem>
+          </DropdownMenu>
+        </ButtonDropdown>
+        <div className="section">
+        
+        </div>
       </div>
     );
   }
